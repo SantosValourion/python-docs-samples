2d3afec481978ae71c95524cc7e44947f43196af
SantosValourion <jasonlongbo@gmail.com>
2021-09-15 11:18:59 -0700

    Update main.py

--- appengine/standard_python3/hello_world/main.py
+++ appengine/standard_python3/hello_world/main.py
@@ -25,7 +25,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello World!'
+    return 'Hello World! I am Conner Ingram!'
 
 
 if __name__ == '__main__':
